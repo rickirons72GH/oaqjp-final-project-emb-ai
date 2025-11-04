1dc156569d5499baad9c1b26682fae609607c375
rickirons72GH <rick.irons72@gmail.com>
2025-11-04 18:20:24 -0500

    more dev changes

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -17,15 +17,24 @@ def emotion_detector(text_to_analyze):
     # Parse the response from the API
     formatted_response = json.loads(response.text)
 
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    # If the response status code is 200, extract information
+    if response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+  
+        anger_score = emotions['anger']
+        disgust_score = emotions['disgust']
+        fear_score = emotions['fear']
+        joy_score = emotions['joy']
+        sadness_score = emotions['sadness']
 
-    anger_score = emotions['anger']
-    disgust_score = emotions['disgust']
-    fear_score = emotions['fear']
-    joy_score = emotions['joy']
-    sadness_score = emotions['sadness']
-
-    dom_emotion = max(emotions, key=emotions.get)
+        dom_emotion = max(emotions, key=emotions.get)
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dom_emotion = None
 
     emotion_scores = { 'anger': anger_score, 'disgust': disgust_score,'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dom_emotion }
 
